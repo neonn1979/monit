fbd5cc08fda409745cb8e10f411bfe378801ff69
gek <118101137+neonn1979@users.noreply.github.com>
2025-05-29 00:23:07 +0800

    Delete replit.nix

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python311
-    pkgs.python311Packages.uvicorn
-    pkgs.python311Packages.fastapi
-    pkgs.python311Packages.pysnmp
-    pkgs.python311Packages.requests
-  ];
-}
