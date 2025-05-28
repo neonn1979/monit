f3a6c055474e6149e28afd405d93a90eff38703e
gek <118101137+neonn1979@users.noreply.github.com>
2025-05-28 12:18:41 +0800

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python311
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.pysnmp
+    pkgs.python311Packages.requests
+  ];
+}
