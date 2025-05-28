cc1672600e5dfffb90cca4ca50c4a0b9b2010409
gek <118101137+neonn1979@users.noreply.github.com>
2025-05-29 00:24:00 +0800

    Delete frontend directory

--- frontend/main.js
+++
@@ -1,9 +0,0 @@
-fetch("/api/status")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("status").innerText =
-      `Устройство ${data.device}: ${data.status} (uptime: ${data.uptime})`;
-  })
-  .catch(err => {
-    document.getElementById("status").innerText = "Ошибка подключения";
-  });
